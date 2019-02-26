a4eb349baf104c9992aa68ca2022334fe1a708cf
Marco Antonio Lopez-Cruz <malctony@hotmail.com>
2019-02-25 22:36:18 -0500

    Update simu_data.R

--- simu_data.R
+++ simu_data.R
@@ -1,22 +1,24 @@
 # Simulates data having n observations containing one response and p predictors.
 # 'meany' and 'sdy': mean and SD of the response
-# 'meanx' and 'sdx': mean and SD of the predictors (all the same)
+# 'meanx' and 'sdx': vectors with mean and SD of the predictors (if length=1: all the same)
 # 'rhox': correlation between two consequtives predictors: cor(X[j],X[j+1])
 # 'rhoxy': correlation between the signal=Xb and the response: cor(Xb,y)
-simData <- function(n,p,meanx=3,sdx=2,meany=5,sdy=1.5,rhox=0.5,rhoxy=0.7,seed=123)
+simData <- function(n,p,meanx=rchisq(p,5),sdx=rchisq(p,1),meany=5,sdy=1.5,rhox=0.5,rhoxy=0.7)
 {
-  set.seed(seed)
+  if(length(meanx)==1)  meanx <- rep(meanx,p)
+  if(length(sdx)==1)  sdx <- rep(sdx,p)
+  
   X <- matrix(nrow=n,ncol=p,NA)
-  X[,1] <- rnorm(n,mean=meanx,sd=sdx)
+  X[,1] <- rnorm(n,mean=meanx[1],sd=sdx[1])
   for(j in 2:p){
-    z <- rnorm(n,mean=meanx,sd=sdx)
+    z <- rnorm(n,mean=meanx[j-1],sd=sdx[j-1])
     X[,j] <- rhox*X[,j-1] + sqrt(1-rhox^2)*z
-    X[,j] <- scale(X[,j],mean(X[,j])-meanx,scale=FALSE)
-  }
+    X[,j] <- sdx[j]*scale(X[,j])+meanx[j]
+  }  
   b <- rnorm(p)
   signal <- X%*%b
   error <- rnorm(n=n,mean=mean(signal),sd=sd(signal))
   y <- rhoxy*signal+sqrt(1-rhoxy^2)*error
-  y <- (y-mean(y))/(sd(y)/sdy) + meany
+  y <- sdy*scale(y)+meany
   return(list(X=X,y=y))
 }
