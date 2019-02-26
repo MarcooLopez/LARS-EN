4061cb13b6ef5c789ff708f5fee34af6b7a78fe3
Marco Antonio Lopez-Cruz <malctony@hotmail.com>
2019-02-25 20:13:01 -0500

    Update simu_data.R

--- simu_data.R
+++ simu_data.R
@@ -5,6 +5,7 @@
 # 'rhoxy': correlation between the signal=Xb and the response: cor(Xb,y)
 simData <- function(n,p,meanx=3,sdx=2,meany=5,sdy=1.5,rhox=0.5,rhoxy=0.7,seed=123)
 {
+  set.seed(seed)
   X <- matrix(nrow=n,ncol=p,NA)
   X[,1] <- rnorm(n,mean=meanx,sd=sdx)
   for(j in 2:p){
